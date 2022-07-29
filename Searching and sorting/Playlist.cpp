bfe2000630d1d24ae4e40a7a4572becd28c419e6
rkcraushan333 <raushank.ug20.ece@nitp.ac.in>
2022-07-29 17:33:39 +0530

    .

--- Searching and sorting/Playlist.cpp
+++ Searching and sorting/Playlist.cpp
@@ -1,38 +1,28 @@
-#include <bits/stdc++.h>
+#include <iostream>
+#include <set>
 using namespace std;
-#define int long long
-// by inforkc
-void inforkc()
+
+int main()
 {
     int n;
     cin >> n;
     vector<int> v(n);
     for (int i = 0; i < n; i++)
         cin >> v[i];
-    unordered_set<int> s;
-    s.insert(v[0]);
-    int i = 0, j = 1, res = 1;
-    while (j <= n)
+    unordered_set<int> h;
+    int i = 0, j = 0, res = 1;
+    while (j < n)
     {
-        if (s.count(v[j]))
+        if (h.count(v[j]))
         {
-            s.clear();
-            i++;
-            s.insert(v[i]);
-            j = i + 1;
+            while (h.count(v[j]))
+            {
+                h.erase(v[i++]);
+            }
         }
-        else
-        {
-            s.insert(v[j]);
-            j++;
-            res = max(res, (int)s.size());
-        }
+        m.insert(v[j++]);
+        res = max(res, h.size());
     }
-    cout << res << endl;
-}
-
-signed main()
-{
-    inforkc();
+    cout << res;
     return 0;
 }
